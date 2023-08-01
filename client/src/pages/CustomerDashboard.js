b1b5aee726db8ad639dbc7a1385250efe6c42163
Mauricio Gomez <gomezb_mauricio@hotmail.com>
2023-08-01 18:25:06 -0500

    updated json file frontend and customer dashboard

---
+++ client/src/pages/CustomerDashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+import image1 from "../assets/images/image1.jpg";
+import image2 from "../assets/images/image2.jpg";
+import image3 from "../assets/images/image3.jpg";
+import image4 from "../assets/images/image4.jpg";
+import image5 from "../assets/images/image5.jpg";
+import image6 from "../assets/images/image6.jpg";
+import image7 from "../assets/images/image7.jpg";
+
+const CustomerDashboard = ()
