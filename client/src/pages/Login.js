941ac4f61b719ce02da6a6949c9cc9973a266cd4
Mauricio Gomez <gomezb_mauricio@hotmail.com>
2023-08-01 12:46:22 -0500

    update new section on login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -54,11 +54,80 @@ const handleChange = event => {
     };
 
     return (
-        
-    )
-
-
-
-}
-
-}
+        <main className="is-mobile is-centered">
+          <div className="column is-half">
+            <div className="card">
+              <header className="card-header">
+                <p class="card-header-title is-centered">
+                  Login
+                </p>
+              </header>
+              <div className="card-content">
+                <form onSubmit={handleFormSubmit}>
+                  <div className="field">
+                    <label className="label">Email</label>
+                    <div className="control has-icons-left has-icons-right">
+                      <input className="input is-success"
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="Enter your email"
+                        defaultValue={formState.email}
+                        onChange={handleChange} />
+                      <span className="icon is-small is-left">
+                        <i className="fas fa-envelope"></i>
+                      </span>
+                      <span className="icon is-small is-right">
+                        <i className="fas fa-exclamation-triangle"></i>
+                      </span>
+                    </div>
+                  </div>
+    
+                  <div className="field">
+                    <label className="label">Password</label>
+                    <p class="control has-icons-left">
+                      <input className="input is-success"
+                        type="password"
+                        id="password"
+                        placeholder="Enter your password"
+                        defaultValue={formState.password}
+                        onChange={handleChange} />
+                      <span className="icon is-small is-left">
+                        <i className="fas fa-lock"></i>
+                      </span>
+                    </p>
+                  </div>
+                  <button className="button is-success" type="submit">
+                    Submit
+                  </button>
+                </form>
+    
+                {error && <div>Login failed</div>}
+              </div>
+            </div>
+            <div className="card">
+              <header className="card-header">
+                <p class="card-header-title is-centered">
+                  Temporary redirects
+                </p>
+              </header>
+              <div className="card-content">
+                <p>Please select one of the buttons to go to the Vendor or Customer dashboards</p>
+              </div>
+              <div className="card-content">
+                <button className="button is-success">
+                  <a href="/vendordashboard">Vendor</a>
+                </button>
+              </div>
+              <div className="card-content">
+                <button className="button is-success">
+                  <a href="/customerdashboard">Customer</a>
+                </button>
+              </div>
+            </div>
+          </div>
+        </main>
+      );
+    };
+    
+    export default Login;
