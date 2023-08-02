debfdf2d34e120fbf6a3698a0952f60f30d2f6ec
Mauricio Gomez <gomezb_mauricio@hotmail.com>
2023-08-01 20:18:44 -0500

    reducers in utils front-end

---
+++ client/src/utils/reducers.js
@@ -0,0 +1,9 @@
+import {
+    ADD_TO_CART,
+    UPDATE_CART_QUANTITY,
+    UPDATE_PROJECTS,
+    UPDATE_CATEGORIES,
+    ADD_MULTIPLE_TO_CART,
+    REMOVE_FROM_CART,
+    CLEAR_CART
+  } from "./actions";
