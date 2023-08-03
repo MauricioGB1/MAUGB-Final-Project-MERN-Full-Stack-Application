e43348f8978ffb9f428e6269fc9c1a4506541cc3
Mauricio Gomez <gomezb_mauricio@hotmail.com>
2023-08-03 18:58:05 -0500

    updates on public folder

--- client/src/pages/VendorDashboard.js
+++ client/src/pages/VendorDashboard.js
@@ -1,1 +1,14 @@
+import React from "react";
+import {Helmet} from "react-helmet";
 
+import image1 from "../assets/images/image1.jpg";
+import image2 from "../assets/images/image2.jpg";
+import image3 from "../assets/images/image3.jpg";
+import image4 from "../assets/images/image4.jpg";
+import image5 from "../assets/images/image5.jpg";
+import image6 from "../assets/images/image6.jpg";
+
+import image7 from "../assets/images/image7.jpg";
+
+import customer1 from "../assets/images/customers/customer1";
+import customer2 from "../assets/images/customers/customer2";
