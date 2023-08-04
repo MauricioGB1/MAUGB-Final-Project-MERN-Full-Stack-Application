07272b7553c525a586da12f6f7384858fd8b35fd
Mauricio Gomez <gomezb_mauricio@hotmail.com>
2023-08-03 19:31:56 -0500

    updates on json file front-end

---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+const User = require("./User");
+const Project = require("./Projects");
+const Firm = require("./Firm");
+const Category = require("./Category");
+const Order = require("./Order");
+
+module.exports = { User, Project, Firm, Category, Order };
